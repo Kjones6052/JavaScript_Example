5167d7a34022c6893fbc43713d26428e69c4d9cd
Kjones6052 <letkylehelpyou@gmail.com>
2024-12-08 09:18:42 -0800

    First Commit: just getting started

---
+++ README.md
@@ -0,0 +1,5 @@
+# **Hello, World!**
+
+This repository contains all the files used to create an example of using JavaScript.
+
+Thank you
---
+++ script.js
@@ -0,0 +1,19 @@
+// External JavaScript File, link to HTML document in <head> section
+
+// This is for single line comments, closes at end of line automatically
+
+/*
+    This is a 
+    multi-line comment,
+    must be closed manually
+*/
+
+// Output methods:
+alert('Hello, External Alert!'); // Using alert => creates window in window
+console.log('Hello, Console!'); // Logging to the console => print()
+document.write('Hello, Document!') // Writing directly to the document
+/*
+    use document.write sparingly or if ever, there is no real reason to 
+    use this instead of something else so use something else whenever
+    possible.
+*/
